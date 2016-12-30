b40be2f29e21eaa141595ef0d49d122a16a42d2b
Aleksandr Tukallo <alex.tukallo@gmail.com>
2016-12-30 10:58:39 +0300

    Minor: either codestyle improved and readme upd

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ The repository contains homeworks for C++ course in ITMO university.
 * `checked` class is a wrapper aroung signed and unsigned integer types, which throws an exception, if overflow happens. This class was written as a homework to learn *Tag Dispatching* and *Traits classes*. 
 
 * `my_bind` function is an implementation of [std::bind](http://en.cppreference.com/w/cpp/utility/functional/bind) function. It uses *move semantics* and *universal references* a lot. Should be compiled with `-std=c++14` flag because some functions return `auto` for better readability.
+
+* `either` is a small version of `std::variant`. It allows to store objects of two different types in one place in memory (while variant offers same functionality but with unlimited number of types). It is in many ways simular to `union` for two different types. Some functions in class are implemented using *double buffering* technique to achieve strong exception safety guarantee. 
--- cpp/either/either.h
+++ cpp/either/either.h
@@ -10,7 +10,6 @@
 #include <memory>
 #include <bits/stdc++.h>
 
-
 struct emplace_left_t
 {
 };
@@ -43,66 +42,63 @@ struct either
 
     ~either();
 
-    either& operator=(either); //inside always move. If rvalue calling side nothing makes
+    either& operator=(either); //inside always move
 
     //accessing
     bool is_left() const;
     Left& left();
-    Left const& left() const; //if Right, UB
+    Left const& left() const; //if Right currently stored, UB
 
     bool is_right() const;
     Right& right();
-    Right const& right() const;
+    Right const& right() const; //if Left currently stored, UB
 
+    //emplace functions satisfy strong exception safety guarantee! (on some conditions)
+    //they are implemented using double buffering technique.
+    // ie firstly space in heap is allocated and copy of current object in data is created, using copy constructor, in heap
+    //  (if copy constructor throws exception, old value is not lost, everything ok)
+    // then state of either is changed to pointer to heap and current value in data is destroyed (suppose destructor no throw)
+    // then we are trying to construct new object in data using given arguments
+    // if constructor fails, unique pointer to heap, where old object was saved, is created, using copy constructor,
+    //  in data (suppose not fails). Everything ok, strong safety provided.
+    // if constructor succeeds, everything is obvious.
+    //same technique is used in swap implementation
+
+    //moves either to left condition, left is created with constructor from these arguments
     template<typename... Args>
-    void
-    emplace(emplace_left_t, Args&& ... args); //moves either to left condition, left is created with this constructor
+    void emplace(emplace_left_t, Args&& ... args);
 
+    //moves either to right condition, right is created with constructor from these args
     template<typename... Args>
     void emplace(emplace_right_t, Args&& ... args);
 
     template<typename... Args>
-    either(emplace_left_t, Args&&... args);
+    either(emplace_left_t, Args&& ... args);
 
     template<typename... Args>
-    either(emplace_right_t, Args&&... args);
+    either(emplace_right_t, Args&& ... args);
 
     //eg either<int, x> a(emplace_right, 1, 2, 3);
 
-    //todo make private
 private:
     either() = delete;
 
     void call_current_destructor();
 
-    std::unique_ptr<Left>* get_left_ptr_ptr();
-   // std::unique_ptr<Left> const* get_left_ptr_ptr();
+    object_stored which;
 
-    object_stored which; 
-
-    typename std::aligned_storage<std::max({sizeof(Left), sizeof(Right), sizeof(Left*)}), 
-                                  std::max({alignof(Left), alignof(Right), alignof(Left*)})>::type data;     
+    typename std::aligned_storage<std::max({sizeof(Left), sizeof(Right), sizeof(Left *)}),
+            std::max({alignof(Left), alignof(Right), alignof(Left *)})>::type data;
 
     template<typename NewType, typename CurType, typename... Args>
-    void emplace_template (CurType old, object_stored newType, Args&&... args);     
+    void emplace_template(CurType old, object_stored newType, Args&& ... args);
 
-    template<typename FLeft, typename FRight, typename Rlhs, typename Rrhs> 
-    friend void swap_impl(either<FLeft, FRight> &lhs, either<FLeft, FRight> &rhs,
-                        std::unique_ptr<Rlhs> ldata, std::unique_ptr<Rrhs> rdata,
-                        object_stored const& ltype, object_stored const& rtype);                    
+    template<typename FLeft, typename FRight, typename Rlhs, typename Rrhs>
+    friend void swap_impl(either<FLeft, FRight>& lhs, either<FLeft, FRight>& rhs,
+                          std::unique_ptr<Rlhs> ldata, std::unique_ptr<Rrhs> rdata,
+                          object_stored const& ltype, object_stored const& rtype);
 };
 
-
-template<typename F, typename Left, typename Right>
-auto apply(F const&, either<Left, Right> const&); //one more overloading needed, same but either not const
-//calls F from Left if left, or Right
-
-//for swap also strong guarantee, same contract as for emplace and operator=
-
-template<typename Left, typename Right>
-void swap(either<Left, Right>&, either<Left, Right>&);
-
-//____________________________________________________________
 //implementation:
 
 template<typename Left, typename Right>
@@ -120,10 +116,10 @@ either<Left, Right>::either(Right right)
 }
 
 template<typename Left, typename Right>
-either<Left,Right>::either(either<Left,Right> const& other)
-{   
+either<Left, Right>::either(either<Left, Right> const& other)
+{
     this->which = other.which;
-    switch(which)
+    switch (which)
     {
         case LEFT:
             new(&data)Left(std::forward<const Left&>(other.left()));
@@ -137,7 +133,7 @@ either<Left,Right>::either(either<Left,Right> const& other)
             break;
         case RIGHT_PTR:
             new(&data)Right(std::forward<const Right&>(other.right()));
-            which = RIGHT; 
+            which = RIGHT;
             break;
     }
 }
@@ -145,11 +141,12 @@ either<Left,Right>::either(either<Left,Right> const& other)
 template<typename Left, typename Right>
 either<Left, Right>::either(either<Left, Right>&& other)
 {
-    if (other.is_left()) {
+    if (other.is_left())
+    {
         which = LEFT;
         emplace(emplace_left, other.left());
-    }
-    else {
+    } else
+    {
         which = RIGHT;
         emplace(emplace_right, other.right());
     }
@@ -160,25 +157,25 @@ either<Left, Right>& either<Left, Right>::operator=(either other)
 {
     if (other.is_left())
         emplace(emplace_left, other.left());
-    else 
+    else
         emplace(emplace_right, other.right());
-    return *this;   
-} 
+    return *this;
+}
 
 template<typename Left, typename Right>
-either<Left,Right>::~either()
+either<Left, Right>::~either()
 {
     call_current_destructor();
 }
 
 template<typename Left, typename Right>
-bool either<Left,Right>::is_left() const
+bool either<Left, Right>::is_left() const
 {
     return which == LEFT || which == LEFT_PTR;
 }
 
 template<typename Left, typename Right>
-bool either<Left,Right>::is_right() const
+bool either<Left, Right>::is_right() const
 {
     return !is_left();
 }
@@ -188,10 +185,10 @@ Left& either<Left, Right>::left()
 {
     assert(which == LEFT_PTR || which == LEFT);
 
-    if(which == LEFT)
-        return *reinterpret_cast<Left*>(&this->data);
+    if (which == LEFT)
+        return *reinterpret_cast<Left *>(&this->data);
     //else LEFT_PTR
-    return **reinterpret_cast<std::unique_ptr<Left>*>(&this->data);
+    return **reinterpret_cast<std::unique_ptr<Left> *>(&this->data);
 }
 
 template<typename Left, typename Right>
@@ -199,10 +196,10 @@ Left const& either<Left, Right>::left() const
 {
     assert(which == LEFT_PTR || which == LEFT);
 
-    if(which == LEFT)
-        return *reinterpret_cast<Left const*>(&this->data);
+    if (which == LEFT)
+        return *reinterpret_cast<Left const *>(&this->data);
     //else LEFT_PTR
-    return **reinterpret_cast<std::unique_ptr<Left> const*>(&this->data);
+    return **reinterpret_cast<std::unique_ptr<Left> const *>(&this->data);
 }
 
 
@@ -211,10 +208,10 @@ Right& either<Left, Right>::right()
 {
     assert(which == RIGHT_PTR || which == RIGHT);
 
-    if(which == RIGHT)
-        return *reinterpret_cast<Right*>(&this->data);
+    if (which == RIGHT)
+        return *reinterpret_cast<Right *>(&this->data);
     //else LEFT_PTR
-    return **reinterpret_cast<std::unique_ptr<Right>*>(&this->data);
+    return **reinterpret_cast<std::unique_ptr<Right> *>(&this->data);
 }
 
 
@@ -223,34 +220,34 @@ Right const& either<Left, Right>::right() const
 {
     assert(which == RIGHT_PTR || which == RIGHT);
 
-    if(which == RIGHT)
-        return *reinterpret_cast<Right const*>(&this->data);
+    if (which == RIGHT)
+        return *reinterpret_cast<Right const *>(&this->data);
     //else LEFT_PTR
-    return **reinterpret_cast<std::unique_ptr<Right> const*>(&this->data);
+    return **reinterpret_cast<std::unique_ptr<Right> const *>(&this->data);
 }
 
 
 template<typename Left, typename Right>
 void either<Left, Right>::call_current_destructor()
 {
-    switch(which)
+    switch (which)
     {
         case LEFT:
-            reinterpret_cast<Left*>(&this->data)->Left::~Left();
+            reinterpret_cast<Left *>(&this->data)->Left::~Left();
             break;
         case RIGHT:
-            reinterpret_cast<Right*>(&this->data)->Right::~Right();
+            reinterpret_cast<Right *>(&this->data)->Right::~Right();
             break;
         case LEFT_PTR:
-            reinterpret_cast<std::unique_ptr<Left>*>(&this->data)->std::unique_ptr<Left>::~unique_ptr<Left>();
+            reinterpret_cast<std::unique_ptr<Left> *>(&this->data)->std::unique_ptr<Left>::~unique_ptr<Left>();
         case RIGHT_PTR:
-            reinterpret_cast<std::unique_ptr<Right>*>(&this->data)->std::unique_ptr<Right>::~unique_ptr<Right>();
+            reinterpret_cast<std::unique_ptr<Right> *>(&this->data)->std::unique_ptr<Right>::~unique_ptr<Right>();
     }
 }
 
 template<typename Left, typename Right>
 template<typename NewType, typename CurType, typename... Args>
-void either<Left, Right>::emplace_template(CurType old, object_stored newType, Args&&... args)
+void either<Left, Right>::emplace_template(CurType old, object_stored newType, Args&& ... args)
 {
     //copy current value to heap
     std::unique_ptr<CurType> tmp = std::make_unique<CurType>(old);
@@ -260,67 +257,67 @@ void either<Left, Right>::emplace_template(CurType old, object_stored newType, A
     try
     {
         //trying to call constructor of new data in storage
-        new (&data) NewType(std::forward<Args> (args)...);                    
+        new(&data) NewType(std::forward<Args>(args)...);
         which = newType; //if success, everything ok
         tmp.reset();
     }
     catch (...)
     {
         //switch to case when in heap
-        if(newType == LEFT)
+        if (newType == LEFT)
             which = LEFT_PTR;
-        if(newType == RIGHT)
+        if (newType == RIGHT)
             which = RIGHT_PTR;
 
         //create ptr in data 
-        new (&data) std::unique_ptr<CurType>(tmp.release());
+        new(&data) std::unique_ptr<CurType>(tmp.release());
         throw;
-    }   
-}                 
+    }
+}
 
 template<typename Left, typename Right>
 template<typename... Args>
-void either<Left, Right>::emplace(emplace_left_t, Args&&... args)
+void either<Left, Right>::emplace(emplace_left_t, Args&& ... args)
 {
     if (which == LEFT)
         emplace_template<Left>(left(), LEFT, args...);
-    else 
-        emplace_template<Left>(right(), LEFT, args...);              
+    else
+        emplace_template<Left>(right(), LEFT, args...);
 }
 
 template<typename Left, typename Right>
 template<typename... Args>
-void either<Left, Right>::emplace(emplace_right_t, Args&&... args)
+void either<Left, Right>::emplace(emplace_right_t, Args&& ... args)
 {
     if (which == LEFT)
         emplace_template<Right>(left(), RIGHT, args...);
-    else 
-        emplace_template<Right>(right(), RIGHT, args...);              
+    else
+        emplace_template<Right>(right(), RIGHT, args...);
 }
 
 template<typename Left, typename Right>
 template<typename... Args>
-either<Left,Right>::either(emplace_left_t, Args&&... args) 
+either<Left, Right>::either(emplace_left_t, Args&& ... args)
 {
     which = LEFT;
-    new (&data) Left(std::forward<Args>(args)...);
+    new(&data) Left(std::forward<Args>(args)...);
 }
 
 template<typename Left, typename Right>
 template<typename... Args>
-either<Left,Right>::either(emplace_right_t, Args&&... args) 
+either<Left, Right>::either(emplace_right_t, Args&& ... args)
 {
     which = RIGHT;
-    new (&data) Right(std::forward<Args>(args)...);
+    new(&data) Right(std::forward<Args>(args)...);
 }
 
 template<typename F, typename Left, typename Right>
 auto apply(F const& f, either<Left, Right> const& cur)
 {
-    if (cur.is_left()) 
+    if (cur.is_left())
         return f(cur.left());
-    else 
-        return f(cur.right());          
+    else
+        return f(cur.right());
 }
 
 template<typename F, typename Left, typename Right>
@@ -329,51 +326,52 @@ auto apply(F const& f, either<Left, Right>& cur)
     if (cur.is_left())
         return f(cur.left());
     else
-        return f(cur.right());          
+        return f(cur.right());
 }
 
-template<typename Left, typename Right, typename Rlhs, typename Rrhs> 
-void swap_impl(either<Left, Right> &lhs, either<Left, Right> &rhs,
-        std::unique_ptr<Rlhs> ldata, std::unique_ptr<Rrhs> rdata,
-        object_stored const& ltype, object_stored const& rtype) {
+template<typename Left, typename Right, typename Rlhs, typename Rrhs>
+void swap_impl(either<Left, Right>& lhs, either<Left, Right>& rhs,
+               std::unique_ptr<Rlhs> ldata, std::unique_ptr<Rrhs> rdata,
+               object_stored const& ltype, object_stored const& rtype)
+{
+    try
+    {
+        lhs.call_current_destructor();
+        new(&lhs.data) Rrhs(*rdata);
+        lhs.which = rtype;
 
-        try
-        {
-            lhs.call_current_destructor();
-            new (&lhs.data) Rrhs(*rdata);
-            lhs.which = rtype;
-
-            rhs.call_current_destructor();
-            new (&rhs.data) Rlhs(*ldata);
-            rhs.which = ltype;
-        }
-        catch (...)
-        {
-            if(lhs.which == LEFT)
-                lhs.which = LEFT_PTR;
-            if(lhs.which == RIGHT)
-                lhs.which = RIGHT_PTR;
-
-            if(rhs.which == LEFT)
-                rhs.which = LEFT_PTR;
-            if(rhs.which == RIGHT)
-                rhs.which = RIGHT_PTR;
-
-            new (&lhs.data) std::unique_ptr<Rlhs>(ldata.release());
-            new (&rhs.data) std::unique_ptr<Rrhs>(rdata.release());
-            throw;
-        }
+        rhs.call_current_destructor();
+        new(&rhs.data) Rlhs(*ldata);
+        rhs.which = ltype;
     }
+    catch (...)
+    {
+        if (lhs.which == LEFT)
+            lhs.which = LEFT_PTR;
+        if (lhs.which == RIGHT)
+            lhs.which = RIGHT_PTR;
 
-template<typename Left, typename Right> 
-void swap(either<Left, Right> &lhs, either<Left, Right> &rhs)
+        if (rhs.which == LEFT)
+            rhs.which = LEFT_PTR;
+        if (rhs.which == RIGHT)
+            rhs.which = RIGHT_PTR;
+
+        new(&lhs.data) std::unique_ptr<Rlhs>(ldata.release());
+        new(&rhs.data) std::unique_ptr<Rrhs>(rdata.release());
+        throw;
+    }
+}
+
+template<typename Left, typename Right>
+void swap(either<Left, Right>& lhs, either<Left, Right>& rhs)
 {
     if (lhs.is_left() && rhs.is_left())
         swap_impl(lhs, rhs, std::make_unique<Left>(lhs.left()), std::make_unique<Left>(rhs.left()), LEFT, LEFT);
     else if (lhs.is_right() && rhs.is_left())
         swap_impl(lhs, rhs, std::make_unique<Right>(lhs.right()), std::make_unique<Left>(rhs.left()), RIGHT, LEFT);
     else if (lhs.is_left() && rhs.is_right())
-        swap_impl(lhs, rhs, std::unique_ptr<Left>(std::make_unique<Left>(lhs.left())), std::unique_ptr<Right>(std::make_unique<Right>(rhs.right())), LEFT, RIGHT);
+        swap_impl(lhs, rhs, std::unique_ptr<Left>(std::make_unique<Left>(lhs.left())),
+                  std::unique_ptr<Right>(std::make_unique<Right>(rhs.right())), LEFT, RIGHT);
     else
         swap_impl(lhs, rhs, std::make_unique<Right>(lhs.right()), std::make_unique<Right>(rhs.right()), RIGHT, RIGHT);
 }
