466fc4200a6c44a9a939a4b6deb64d7d223cfbc4
Aleksandr Tukallo <alex.tukallo@gmail.com>
2017-03-16 00:11:07 +0300

    Minor: some inlines united in one, close to palignr

--- asm/asm_inlines/word_counter.cpp
+++ asm/asm_inlines/word_counter.cpp
@@ -30,8 +30,11 @@ std::string to_string_by_bytes(__m128i var, bool is_signed = false)
     return ret;
 }
  
-uint32_t count_words(char const *arr, size_t size)
+uint32_t count_words(std::string const& str)
 {
+    char const *arr = str.c_str();
+    size_t size = str.size();
+
     __m128i space_mask_reg = _mm_set_epi8(32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32);
     __m128i shifted_mask_reg = _mm_set_epi8(255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
@@ -59,6 +62,7 @@ uint32_t count_words(char const *arr, size_t size)
     }
 
     //some crutches
+    if(cur_position == size) return ans;
     if ((is_whitespace && *(arr + cur_position) != ' ' && cur_position != 0)
                   || (cur_position == 0 && *arr != ' ')) ans++;
     if(cur_position != 0 && *arr != ' ') ans++;
@@ -67,25 +71,41 @@ uint32_t count_words(char const *arr, size_t size)
     LOG("\nNow is aligned, ans: " + std::to_string(ans) +  " string left:\n" + std::string(cur_position + arr) + "\n\n");
 
     //main part, using vectorization, every step is 16 bytes
-    __m128i store; //variable is used to store bytes got in `for` loop
-    store = _mm_set_epi32(0, 0, 0, 0);
+    __m128i store = _mm_set_epi32(0, 0, 0, 0); //variable is used to store bytes got in `for` loop
+    __m128i cur_cmp, next_cmp; //variables store current 16 bytes and next 16 bytes respectively after comparing them with space mask
+    __m128i trasher; //todo delete later  
+
     size_t end_of_vectorization = size - (size - cur_position) % 16;
-    for(size_t i = cur_position; i < end_of_vectorization; i += 16) //while i < new size floor 16
+    //if more than two steps are made
+    if(end_of_vectorization - cur_position >= 32)
     {
-        __m128i trasher; //todo delete later         
+        //get second 16 bytes of string from memory and compare them with space mask
+        __asm__("movdqa\t" "(%2), %1\n"
+                "pcmpeqb\t" "%1, %0"
+                :"=x"(next_cmp), "=x"(trasher)
+                :"r"(arr + cur_position), "0"(space_mask_reg));
+    }
+
+    for(size_t i = cur_position; i < end_of_vectorization - 16; i += 16) //while i < new size (floor 16) - 16 (- 16 because, always reading next bytes)
+    {
         //here we write many asm inlines for debugging, later unite in one
 
-        __m128i part_of_string;
+        __m128i next_part;
+        cur_cmp = next_cmp;
         //mov part of string to register
         __asm__("movdqa\t" "(%1), %0"
-                :"=x"(part_of_string)
-                :"r"(arr + i));
+                :"=x"(next_part)
+                :"r"(arr + i + 16));
 
-        __m128i cmp_result;
         //compare part of string with space mask
         __asm__("pcmpeqb\t" "%1, %0"
-                :"=x"(cmp_result), "=x"(trasher)
-                :"1"(part_of_string), "0"(space_mask_reg));
+                :"=x"(next_cmp), "=x"(trasher)
+                :"1"(next_part), "0"(space_mask_reg));
+
+        LOG("Curcmp" + to_string_by_bytes(cur_cmp) + "\n");
+        LOG("Nexcmp" + to_string_by_bytes(next_cmp) + "\n");
+
+        __m128i cmp_result = cur_cmp;
 
         //shift right???? by one
         __m128i cmp_result_sll_with_zero;
@@ -103,6 +123,17 @@ uint32_t count_words(char const *arr, size_t size)
         __m128i result; //in our byte there is space,
         //  but in right neigbour there is no space (ie 1 if space before start of word)
         //result = not cmp_res_shifted and cmp_res
+
+        uint32_t msk;
+
+        /*__asm__("pandn\t" "%3, %2\n"
+                "psubsb\t" "%2, %4\n"
+                "paddusb\t" "%4, %0\n"
+                "pmovmskb\t" "%0, %1" 
+                :"=x"(store), "=r"(msk) 
+                :"x"(cmp_result_shifted), "x"(cmp_result), "x"(_mm_set_epi32(0, 0, 0, 0)), "0"(store)
+                :"2", "4");*/
+
         __asm__("pandn\t" "%1, %0"
                 :"=x"(result), "=x"(trasher) 
                 :"1"(cmp_result), "0"(cmp_result_shifted));
@@ -121,7 +152,6 @@ uint32_t count_words(char const *arr, size_t size)
                 :"1"(positive_result), "0"(store));
         LOG("Store " + to_string_by_bytes(store, false) + "\n");
 
-        uint32_t msk;
         __asm__("pmovmskb\t" "%1, %0"
             :"=r"(msk), "=x"(trasher)
             :"x"(store), "0"(msk));
@@ -189,9 +219,9 @@ void do_tests(size_t num = 50)
     std::cout << std::time(0) << std::endl;
     for(size_t i = 0; i < num; i++)
     {
-        std::string str = get_random_string();
+        std::string str = get_random_string(5, 50, 2);
         std::cerr << "New string:\n" << str << std::endl << std::endl;
-        uint32_t fast_ans = count_words(str.c_str(), str.size());
+        uint32_t fast_ans = count_words(str);
         uint32_t slow_ans = count_words_slowly(str);
 
         std::cerr << "Slow ans: " << slow_ans << ", fast ans: " << fast_ans << std::endl; 
@@ -204,11 +234,11 @@ void do_tests(size_t num = 50)
 
 int main()
 {
-        char* str = (char*)"abc bbbbbb bbbbb bbbbbbbbbbbbbbbbb";
-        char* str_strange = (char*)"wwwwwww        wwwwwwwwwww ww www        www w wwwww";
-        char* str_strange1 = (char*)"aaaaaaaaaa _aaaaaaaaaaaaaaaaaaaa";
+        std::string str("abc bbbbbb bbbbb bbbbbbbbbbbbbbbbb");
+        std::string str_strange("wwwwwww        wwwwwwwwwww ww www        www w wwwww     ww ww www        www w wwwww");
+        std::string str_strange1("aaaaaaaaaa _aaaaaaaaaaaaaaaaaaaa");
 
-        do_tests();
+        count_words(str_strange);
 
         return 0;
 }
