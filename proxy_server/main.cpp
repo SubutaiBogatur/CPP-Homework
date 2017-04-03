41b5bf3cad1e18ee7b6c6c0586f03bd4b1e0e259
Aleksandr Tukallo <alex.tukallo@gmail.com>
2017-04-04 00:42:52 +0300

    echo_server.cpp

--- proxy_server/main.cpp
+++ proxy_server/main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 #include <netinet/in.h>
 
-void start_echo_server();
 
-int main()
-{
-    std::cout << "Main started!" << std::endl;
-    start_echo_server();
-    return 0;
-}
+
 
