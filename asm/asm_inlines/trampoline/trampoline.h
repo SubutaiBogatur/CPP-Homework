002d53f36d4a6527b815c751da2f8722603d6251
Aleksandr Tukallo <alex.tukallo@gmail.com>
2017-06-24 00:04:50 +0300

    Minor: new code style approved
    Now assembly inlines have syntax of usual assembly inlines, which seems great for me

--- asm/asm_inlines/trampoline/trampoline.h
+++ asm/asm_inlines/trampoline/trampoline.h
@@ -3,8 +3,8 @@
 
 #include "argument_types.h"
 #include "allocator.h"
-#include <stdlib.h>
-#include <sys/mman.h>
+#include <string>
+#include <iostream>
 
 template <typename T>
 struct trampoline;
@@ -16,6 +16,27 @@ template <typename T, typename ... Args>
 struct trampoline<T (Args ...)>
 {
 private:
+    void *page;
+    char *ip; //current instruction pointer
+
+    void* func_obj;
+    void (*deleter)(void*);
+
+    struct imm
+    {
+        union
+        {
+            void *addr;
+            int32_t i;
+        };
+        size_t size;
+
+
+
+        imm(void *addr, size_t size):size(size), addr(addr){}
+        imm(int32_t i, size_t size):size(size), i(i){}
+    };
+
     const char* shifts[6] = {
         "\x48\x89\xfe" /*mov rsi rdi*/,
         "\x48\x89\xf2" /*mov rdx rsi*/,
@@ -25,137 +46,129 @@ private:
         "\x41\x51" /*push %%r9;*/
     };
 
-    void add(char* &p, const char* command) {
-        for (const char *i = command; *i; i++) {
-            *(p++) = *i;
-        }
+    //function to add instructions
+    void add() {}
+
+    void add(std::string command)
+    {
+        for (const char *i = command.c_str(); *i; i++) *(ip++) = *i;
+    }
+
+    void add(imm im)
+    {
+        *(void**)ip = im.addr;
+        ip += im.size;
+    }
+
+    template<typename ... A>
+    void add(std::string str, A... args)
+    {
+        add(str);
+        add(args...);
+    }
+
+    template<typename ... A>
+    void add(imm im, A... args)
+    {
+        add(im);
+        add(args...);
     }
 
 public:
     template <typename F>
     trampoline(F func) : func_obj(new F(std::move(func))), deleter(my_deleter<F>)
     {
-        code = allocator::get_instance().malloc();
-        char* pcode = (char*)code;
+        page = allocator::get_instance().malloc();
+        ip = (char*)page;
 
         if (args_types<Args ...>::INTEGER < 6)
         {
-            /* shift each param for the next free register */
-            for (int i = args_types<Args ...>::INTEGER - 1; i >=0 ; i--) add(pcode, shifts[i]);
+            // shift all the arguments
+            for (int i = args_types<Args ...>::INTEGER - 1; i >=0 ; i--) add(shifts[i]);
 
-            add(pcode,"\x48\xbf");
-            *(void**)pcode = func_obj; //mov rdi, imm
-            pcode += 8;
-
-            /* set ret function ptr */
-            add(pcode, "\x48\xb8"); // mov rax, imm
-            *(void**)pcode = (void*)&do_call<F>;
-            pcode += 8;
-
-            /* jump to ret function */
-            add(pcode, "\xff\xe0"); //jmp rax
+            add("\x48\xbf", imm(func_obj, 8),                   // mov rdi, ptr_to_func_obj
+                "\x48\xb8", imm((void *)&do_call<F>, 8),        // mov rax, address_of_do_call
+                "\xff\xe0");                                    // jmp rax
         }
         else
         {
-            /*---------------- place arguments ----------------*/
 
-            // save into r11 previous top of stack adress to return it after executing do_call
-            add(pcode, "\x4c\x8b\x1c\x24"); //mov r11 [rsp]
+            // save return address, which is in on top of stack
+            add("\x4c\x8b\x1c\x24");                            //mov r11 [rsp]
 
-            // push 6'th argument to stack, shift other arguments rdi is free now
-            for (int i = 5 ; i >= 0; i--) add(pcode, shifts[i]);
+            // shift all the args and push last to the stack
+            for (int i = 5 ; i >= 0; i--) add(shifts[i]);
 
-            // 5 INTEGER args in r[9,8], rsi, rcx, rdx, 1 on stack, <= eight SSE args in xmm[0..7] stack_size - amount of arguments to place on stack
+            // maximum possible size of arguments on stack
             int stack_size = 8 * (args_types<Args ...>::INTEGER - 5 + std::max(args_types<Args ...>::SSE - 8, 0));
 
+            // now let's get stack ready for new call
+            add("\x48\x89\xe0",                                 //mov rax, rsp
+                "\x48\x05", imm(stack_size, 4),                 //add rax, stack_size
+                "\x48\x81\xc4", imm(8, 4));                     //add rsp, 8
 
-            /*--------------- shifting arguments --------------*/
+            char* label_1 = ip; //cycle for shifting
 
-            // rax on the top of current stack
-            add(pcode, "\x48\x89\xe0"); //mov rax, rsp
-
-            // set rax onto the zero argument on stack
-            add(pcode,"\x48\x05"); //add rax, stack_size
-            *(int32_t*)pcode = stack_size;
-            pcode += 4;
-
-            // shift rsp to free place to provide shifting of args on stack using rsp
-            add(pcode,"\x48\x81\xc4"); //add rsp, 8
-            *(int32_t*)pcode = 8;
-            pcode += 4;
-
-            //need to shift all args by 1, to free place for return function adress
-            //do shifting in cycle using label_1
-            char* label_1 = pcode;
-
-            // compare rax and rsp, to understand if all arguments were shifted
-            add(pcode,"\x48\x39\xe0"); //cmp rax, rsp
-
-            // if all arguments have been already shifted go to label_2
-            add(pcode, "\x74"); //je
-
-            char* label_2 = pcode;
-            pcode++;
-            add(pcode, "\x48");
+            add("\x48\x39\xe0",                                 //cmp rax, rsp
+                "\x74\x13");                                    //je 13  (*sorry very much, but this does mean jumping 13 bytes lower*)
+            add( "\x48");
 
 
             /*----------------- shifting argument -------------------*/
             {
                 // rsp point to free place, set it to arg we want to shift add rsp,
-                add(pcode,"\x48\x81\xc4\x08");
-                pcode += 3;
+                add("\x48\x81\xc4\x08");
+                ip += 3;
                 // save arg we want to shift in rdi, it was free before this mov rdi,[rsp]
-                add(pcode, "\x48\x8b\x3c\x24");
-
-                // shift arg by pusing saved in rdi argument onto free place on stack mov [rsp-0x8],rdi
-                add(pcode, "\x48\x89\x7c\x24\xf8");
+                add( "\x48\x8b\x3c\x24",
+                     "\x48\x89\x7c\x24\xf8");
 
                 // jupm cause need to shift all args in cycle jmp
-                add(pcode, "\xeb");
-                *pcode = label_1 - pcode - 1;
-                pcode++;
+                add( "\xeb");
+                *ip = label_1 - ip - 1;
+                ip++;
             }
-            *label_2 = pcode - label_2 - 1;
+//            *label_2 = ip - label_2 - 1;
 
             /*----------- working with rsp, calling function ----------*/
 
             //move saved in r11 return adress onto the free place mov [rsp],r11
-            add(pcode, "\x4c\x89\x1c\x24");
+            add( "\x4c\x89\x1c\x24");
 
             //update rsp sub rsp,...
-            add(pcode,"\x48\x81\xec");
-            *(int32_t*)pcode = stack_size;
-            pcode += 4;
+            add("\x48\x81\xec");
+            *(int32_t*)ip = stack_size;
+            ip += 4;
 
             /*  move executing function ptr to the first register */
-            add(pcode,"\x48\xbf");
-            *(void**)pcode = func_obj;
-            pcode += 8;
+            add("\x48\xbf");
+            *(void**)ip = func_obj;
+            ip += 8;
 
             /* set ret function ptr */
-            add(pcode, "\x48\xb8");
-            *(void**)pcode = (void*)&do_call<F>;
-            pcode += 8;
+            add( "\x48\xb8");
+            *(void**)ip = (void*)&do_call<F>;
+            ip += 8;
 
             // calling function, in rax call rax
-            add(pcode,"\xff\xd0");
+            add("\xff\xd0");
 
             // removing 6'th arg from stack
-            add(pcode,"\x41\x59");
+            add("\x41\x59");
 
             /* updating rsp */
 
                 //saving into r11 adress of current rsp plus shift on curent size of stack have one less argument in stack cause of one arument have already been deleted from stack mov r11,[rsp+const]
-                add(pcode,"\x4c\x8b\x9c\x24");
-                *(int32_t*)pcode = stack_size - 8;
-                pcode += 4;
+                add("\x4c\x8b\x9c\x24");
+                *(int32_t*)ip = stack_size - 8;
+                ip += 4;
 
                 //set corrrect value previously stored in r11 to rsp mov [rsp],r11
-                add(pcode, "\x4c\x89\x1c\x24");
+                add( "\x4c\x89\x1c\x24");
 
 
             /* FINALLY RETURN */
-            add(pcode,"\xc3");
+            add("\xc3");
         }
     }
 
@@ -165,7 +178,7 @@ public:
     }
 
     T (*get() const)(Args ... args) {
-        return (T(*)(Args ... args))code;
+        return (T(*)(Args ... args))page;
     }
 
     trampoline& operator=(const trampoline& other) = delete;
@@ -182,7 +195,7 @@ public:
     {
         std::cout << "in move constructor\n";
         func_obj = other.func_obj;
-        code = other.code;
+        page = other.page;
         deleter = other.deleter;
         other.func_obj = nullptr;
     }
@@ -192,13 +205,11 @@ public:
     ~trampoline()
     {
         if (func_obj) deleter(func_obj);
-        allocator::get_instance().free(code);
+        allocator::get_instance().free(page);
     }
 
 private:
-    void* func_obj;
-    void* code;
-    void (*deleter)(void*);
+
 
     template <typename F>
     static void my_deleter(void* func_obj) {
